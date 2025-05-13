85d6856b1aba1f9d7bba29be99c455342e594b1a
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-05-12 21:40:06 -0600

    Javier [FEAT]: Implement CQRS By MediatoR

---
+++ Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Domain.Bookings;
+using MediatR.NotificationPublishers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) 
+    {
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(AssemblyReference.Assembly));
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
