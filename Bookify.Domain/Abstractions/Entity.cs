d6d8ea78bdbee1b9ca8d6820a4adec6ee57328ae
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-06-24 23:26:16 -0600

    [Feat]: Enrich DDD Entity Abstract Class

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -9,4 +9,59 @@ public abstract class Entity<TModel>
     }
 
     public Guid Id { get; init; }
+
+    public bool IsTransient() => Object.Equals(Id, default(Guid));
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TModel> entity && Equals(entity);
+    }
+
+    public bool Equals (Entity<TModel>? other)
+    {
+        if (other is null || other.IsTransient() || this.IsTransient())
+        {
+            return false;
+        }
+
+        return Object.Equals(Id, other.Id);
+    }
+
+    int? _requestedHashCode;
+    public override int GetHashCode()
+    {
+        if(!IsTransient())
+        {
+            if(!_requestedHashCode.HasValue)
+            {
+                _requestedHashCode = HashCode.Combine(Id);
+            }
+
+            return _requestedHashCode.Value;
+
+        }
+        return base.GetHashCode();
+
+    }
+
+    public static bool operator ==(Entity<TModel>? left, Entity<TModel>? right)
+    {
+        if (left is null && right is null)
+        {
+            return true;
+        }
+
+        if (left is null || right is null)
+        {
+            return false;
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity<TModel>? left, Entity<TModel>? right)
+    {
+        return !(left == right);
+    }
+
 }
