59ded28481c3c8cb4ae8b1b6ee2e1bdb10c0aca1
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-06-25 23:31:46 -0600

    [FEAT]: Define Apartment Entity using DDD

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,18 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    public static readonly Currency USD = new("USD");
+    public static readonly Currency EUR = new("EUR");
+    internal static readonly Currency None = new(string.Empty);
+
+    private Currency(string code) => Code = code;
+
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code) => All.FirstOrDefault(c => c.Code.Equals(code, StringComparison.OrdinalIgnoreCase))
+        ?? throw new ApplicationException($"Currency with code '{code}' does not exist.");
+
+    public static readonly IReadOnlyCollection<Currency> All = [USD, EUR];
+
+}
---
+++ Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,26 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Money(decimal Amount, Currency Currency) 
+{
+
+    public static Money operator +(Money left, Money rigth) 
+    {
+        if (left.Currency != rigth.Currency) 
+        {
+            throw new InvalidOperationException("Currencies have to be equal");
+        }
+
+        return new Money(left.Amount + rigth.Amount, left.Currency);
+    }
+
+    public static Money operator -(Money left, Money rigth) 
+    {
+        if (left.Currency != rigth.Currency) 
+        {
+            throw new InvalidOperationException("Currencies have to be equal");
+        }
+        return new Money(left.Amount - rigth.Amount, left.Currency);
+    }
+
+    public Money Zero() => new(0, Currency.None);
+}
