07b50cfe48b914aa76c753a98c9f4d773056ae02
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-06-26 22:55:04 -0600

    [FEAT]: Add Entity, Value Objects and Events for User

---
+++ Bookify.Domain/Users/Events/UserCreatedDomainEvent.cs
@@ -0,0 +1,5 @@
+﻿using Bookify.SharedKernel;
+
+namespace Bookify.Domain.Users.Events;
+
+public sealed record UserCreatedDomainEvent(Guid UserId) : IDomainEvent;
---
+++ Bookify.Domain/Users/User.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
+
+namespace Bookify.Domain.Users;
+
+public sealed class User : Entity<User>
+{
+    private User(
+        Guid id,
+        FirstName firstName,
+        LastName lastName, 
+        Email email)
+        :base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public FirstName FirstName { get; private set; } 
+    public LastName LastName { get; private set; } 
+    public Email Email { get; private set; }
+
+    public static User Create(FirstName firstName, LastName lastName, Email email) 
+    {
+        var user = new User(Guid.CreateVersion7(), firstName, lastName, email);
+
+        user.RaiseDomainEvent( new UserCreatedDomainEvent(UserId: user.Id ));
+
+        return user;
+    }
+}
