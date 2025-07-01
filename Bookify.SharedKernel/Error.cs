6bb17b126fdc82ff9798ffd7aa83cd2133d30bc3
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-06-30 23:30:49 -0600

    [FEAT]: Add Result Pattern classes

---
+++ Bookify.SharedKernel/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.SharedKernel;
+
+public record Error(string Code, string Message)
+{
+    public static Error NullValue => new("NullValue", "Value cannot be null");
+    public static Error None => new(string.Empty, string.Empty);
+
+}
