9feb9f02a9cace85cc4173f07d4df2cf12ff4e13
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-07-02 23:21:46 -0600

    [FEAT]: Add Review entity, Value Objects and Events

---
+++ Bookify.Domain/Reviews/Review.cs
@@ -0,0 +1,59 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Bookify.SharedKernel;
+
+namespace Bookify.Domain.Reviews;
+
+public sealed class Review : Entity<Review>
+{
+    public Review(
+        Guid id,
+        Guid apartmentId,
+        Guid bookingId, 
+        Guid userId, 
+        Rating rating, 
+        Comment comment, 
+        DateTime createdAt)
+        :base(id)
+    {
+        ApartmentId = apartmentId;
+        BookingId = bookingId;
+        UserId = userId;
+        Rating = rating;
+        Comment = comment;
+        CreatedAt = createdAt;
+    }
+
+    public Guid ApartmentId { get; private set; }
+    public Guid BookingId { get; private set; }
+    public Guid UserId { get; private set; }
+    public Rating Rating { get; private set; }
+    public Comment Comment { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public static Result<Review> Create(
+        Booking booking,
+        Rating rating,
+        Comment comment,
+        DateTime createdOnUtc)
+    {
+        if (booking.Status != BookingStatus.Completed)
+        {
+            return Result.Failure<Review>(ReviewErrors.NotEligible);
+        }
+
+        var review = new Review(
+            Guid.NewGuid(),
+            booking.ApartmentId,
+            booking.Id,
+            booking.UserId,
+            rating,
+            comment,
+            createdOnUtc);
+
+        review.RaiseDomainEvent(new ReviewCreatedDomainEvent(review.Id));
+
+        return review;
+    }
+
+}
