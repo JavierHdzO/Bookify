563f3170abcc26e1aee59a31d7734e71fc124589
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-07-03 22:43:15 -0600

    [FEAT]: Add Validation behavior for command request objects that implement IBaseCommand

---
+++ Bookify.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Application.Exceptions;
+
+public sealed class ValidationException : Exception
+{
+    public IEnumerable<ValidationError> Errors { get; }
+
+    public ValidationException(IEnumerable<ValidationError> errors)
+        : base("One or more validation failures have occurred.")
+    {
+        Errors = errors;
+    }
+}
