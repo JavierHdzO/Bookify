a00032473816eb88a9c0d2bf65d9f446e8bbca2f
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-07-03 22:44:09 -0600

    [FEAT]: Define beahaviors and validation in the DI container

--- Bookify.Application/DependecyInjection.cs
+++ Bookify.Application/DependecyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Bookify.Domain.Bookings;
+﻿using Bookify.Application.Abstractions.Behaviors;
+using Bookify.Domain.Bookings;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -12,7 +14,11 @@ public static class DependecyInjection
         services.AddMediatR(cfg => 
         {
             cfg.RegisterServicesFromAssembly(AssemblyReference.Assembly);
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssembly(AssemblyReference.Assembly, includeInternalTypes: true);
 
         services.AddTransient<PricingService>();
 
