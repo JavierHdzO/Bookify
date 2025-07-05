61ab21ef1430eb66733e84bceb2140eb70f974b6
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-07-04 23:15:54 -0600

    [FEAT]: Add Assembly reference and Add sln

---
+++ Bookify.Infrastructure/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Bookify.Infrastructure;
+
+internal static class AssemblyReference
+{
+    public static Assembly Assembly => typeof(AssemblyReference).Assembly;
+}
