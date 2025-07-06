2a04e3ea7022b1d72836c4841c88b9b65204259d
JavierHdzO <fco_hernandez1705@hotmail.com>
2025-07-05 18:38:43 -0600

    [FEAT]: Add custom control for Concurrency exception

--- Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Exceptions;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
@@ -57,16 +58,23 @@ internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBook
             return Result.Failure<Guid>(BookingErrors.Overlap);
         }
 
-        var booking = Booking.Reserve(
+        try
+        {
+            var booking = Booking.Reserve(
             apartment,
             user.Id,
             duration,
             utcNow: _dateTimeProvider.UtcNow,
             _pricingService);
 
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return booking.Id;
+            return booking.Id;
+        }
+        catch (ConcurrencyException) 
+        {
+            return Result.Failure<Guid>(BookingErrors.Overlap);
+        }
 
     }
 } 
